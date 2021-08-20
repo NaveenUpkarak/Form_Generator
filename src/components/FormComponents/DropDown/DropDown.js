a2bf5077533eea83824af6caa9eedce17fcc3b56
Naveen <naveen19perpaga91@gmail.com>
2021-08-20 17:51:55 +0530

    some changes

--- src/components/FormComponents/DropDown/DropDown.js
+++ src/components/FormComponents/DropDown/DropDown.js
@@ -4,19 +4,18 @@ import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import styles from "./styles.module.css"
 
 export default function DropDown({fields}) {
-
     const dropdownContent = fields.dropdown_content
 
-  return (
-    <div className={styles.dropdown}>
-    <div>{fields.label}</div>
-      <input style={{padding: "5px", width: "100px"}} value={fields.value}></input>
-      <FontAwesomeIcon style={{ marginLeft: "-15px" }} icon={faCaretDown} />
-      <div className={styles.dropContent}>
-        <a href="#">{dropdownContent.link_1}</a>
-        <a href="#">{dropdownContent.link_2}</a>
-        <a href="#">{dropdownContent.link_3}</a>
+    return (
+      <div className={styles.dropdown}>
+      <div>{fields.label}</div>
+        <input style={{padding: "5px", width: "100px"}} value={fields.value}></input>
+        <FontAwesomeIcon style={{ marginLeft: "-15px" }} icon={faCaretDown} />
+        <div className={styles.dropContent}>
+          <a href="#">{dropdownContent.link_1}</a>
+          <a href="#">{dropdownContent.link_2}</a>
+          <a href="#">{dropdownContent.link_3}</a>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
